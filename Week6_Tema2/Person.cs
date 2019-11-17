5aa62e4ff24f05640777c9e04d9ba2c4a46b0068
Vera <Vera@DESKTOP-PBQVJHH>
2019-11-17 14:36:59 +0200

    update

---
+++ Week6_Tema2/Person.cs
@@ -0,0 +1,26 @@
+﻿namespace Week6_Tema2
+{
+    partial class Program
+    {
+        public class Person
+        {
+            public Person(string name)
+            {
+                this.Name = name;
+            }
+            public string Name { get; set; }
+            public int? Age { get; set; }
+
+            public override string ToString()
+            {
+                if (this.Age == null)
+                {
+                    return $"This person's name is: {this.Name} but age is unknown";
+                }
+                return $"This person's name is {this.Name} and is {this.Age} years old";
+            }
+        }
+
+        
+    }
+}
---
+++ Week6_Tema2/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week6_Tema2
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            var s = new Student("Vera", "", "Gritcu",123456, "Iasi", "0752390308", "Vera.Gritcu@yahoo.com",
+                University.Al_I_Cuza, Faculty.FEAA, Speciality.Accounting, "Course" );
+
+            var s2 = s.Clone();
+
+            s.EMail = "V.G@gmail.com";
+
+            var p = new Person("Vera");
+            Console.WriteLine(p.ToString());
+        }
+
+        public enum University
+        {
+            Al_I_Cuza,
+            Gheorge_Asachi,
+            Ion_Ionescu_de_la_Brad
+        }
+        
+        public enum Faculty
+        {
+            FEAA,
+            Biology,
+            Mechanics,
+            Agricultura,
+            Horticultura            
+        }
+
+        public enum Speciality
+        {
+            Marketing,
+            Statistics,
+            Accounting,
+            Biochimics,
+            Ecology_and_Environment_Protection,
+            Engines,
+            Consumer_and_Environment_Protection,
+            Agricultural_Biotechnologies
+
+        }
+
+        
+    }
+}
